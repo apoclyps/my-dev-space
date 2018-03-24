ca6712324767220e47445b0a8f5c59f19b8132b1
Kyle Harrison <kyle90adam@hotmail.com>
2018-03-24 12:25:02 +0000

    [ui-client] Adding create react app and docker support (#2)
    * Generating react app
    
    * Adding docker setup
    
    * Adding nginx config
    
    * Adding initial ui

---
+++ services/client/src/App.jsx
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import axios from 'axios';
+
+import UsersList from './components/UsersList';
+import About from './components/About';
+import NavBar from './components/NavBar';
+import Form from './components/forms/Form';
+import Logout from './components/Logout';
+import UserStatus from './components/UserStatus';
+import Message from './components/Message';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      title: 'Platform',
+      isAuthenticated: false,
+      messageName: null,
+      messageType: null,
+    };
+    this.logoutUser = this.logoutUser.bind(this);
+    this.loginUser = this.loginUser.bind(this);
+    this.createMessage = this.createMessage.bind(this);
+    this.removeMessage = this.removeMessage.bind(this);
+  }
+  componentWillMount() {
+    if (window.localStorage.getItem('authToken')) {
+      this.setState({ isAuthenticated: true });
+    };
+  };
+  componentDidMount() {
+    this.getUsers();
+  };
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { this.setState({ users: res.data.data.users }); })
+    .catch((err) => {  });
+  };
+  logoutUser() {
+    window.localStorage.clear();
+    this.setState({ isAuthenticated: false });
+  };
+  loginUser(token) {
+    window.localStorage.setItem('authToken', token);
+    this.setState({ isAuthenticated: true });
+    this.getUsers();
+    this.createMessage('Welcome!', 'success');
+  };
+  createMessage(name='Sanity Check', type='success') {
+    this.setState({
+      messageName: name,
+      messageType: type
+    });
+    setTimeout(() => {
+      this.removeMessage();
+    }, 3000);
+  };
+  removeMessage() {
+    this.setState({
+      messageName: null,
+      messageType: null
+    });
+  };
+  render() {
+    return (
+      <div>
+        <NavBar
+          title={this.state.title}
+          isAuthenticated={this.state.isAuthenticated}
+        />
+        <div className="container">
+          {this.state.messageName && this.state.messageType &&
+            <Message
+              messageName={this.state.messageName}
+              messageType={this.state.messageType}
+              removeMessage={this.removeMessage}
+            />
+          }
+          <div className="row">
+            <div className="col-md-6">
+              <br/>
+              <Switch>
+                <Route exact path='/' render={() => (
+                  <UsersList
+                    users={this.state.users}
+                  />
+                )} />
+                <Route exact path='/about' component={About}/>
+                <Route exact path='/register' render={() => (
+                  <Form
+                    formType={'register'}
+                    isAuthenticated={this.state.isAuthenticated}
+                    loginUser={this.loginUser}
+                    createMessage={this.createMessage}
+                  />
+                )} />
+                <Route exact path='/login' render={() => (
+                  <Form
+                    formType={'login'}
+                    isAuthenticated={this.state.isAuthenticated}
+                    loginUser={this.loginUser}
+                    createMessage={this.createMessage}
+                  />
+                )} />
+                <Route exact path='/logout' render={() => (
+                  <Logout
+                    logoutUser={this.logoutUser}
+                    isAuthenticated={this.state.isAuthenticated}
+                  />
+                )} />
+                <Route exact path='/status' render={() => (
+                  <UserStatus
+                    isAuthenticated={this.state.isAuthenticated}
+                  />
+                )} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  };
+};
+
+export default App;
