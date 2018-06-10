e1c01ca47c15631964a83957177507b35a81b415
Kyle Harrison <kyle90adam@hotmail.com>
2018-06-10 13:29:04 +0100

    Adds scripts to retrieve data from Eventbrite & Meetup (#149)

---
+++ scripts/src/eventbrite.py
@@ -0,0 +1,51 @@
+# std lib import
+import os
+import pprint
+import requests
+import json
+
+# 3rd party imports
+from eventbrite import Eventbrite
+
+
+EVENTBRITE_API_TOKEN = os.getenv("EVENTBRITE_API_TOKEN")
+EVENTS_ENDPOINT = os.getenv("EVENTS_ENDPOINT")
+
+client = Eventbrite(EVENTBRITE_API_TOKEN)
+
+
+def _transform_event(event):
+    return {
+        "created": event["created"],
+        "description": event["description"]["text"],
+        "event_url": event["url"],
+        "photo_url": "",
+        "id": event["id"],
+        "name": event["name"]["text"],
+        "group_name": event["name"]["text"],
+        "member_type": "ticket",
+        "status": event["status"],
+        "time": event["start"]["utc"],
+    }
+
+
+def _post_payloads(payloads):
+    responses = []
+    for payload in payloads:
+        r = requests.post(
+            EVENTS_ENDPOINT,
+            headers={"Content-type": "application/json"},
+            data=json.dumps(payload),
+        )
+        responses.append(r)
+
+
+if __name__ == "__main__":
+    response = client.get(
+        "/events/search/?q=technology&location.address=belfast&sort_by=-date"
+    )
+    events = response["events"]
+
+    transformed_events = [_transform_event(e) for e in events]
+
+    _post_payloads(transformed_events)
---
+++ scripts/src/meetup.py
@@ -0,0 +1,57 @@
+# std lib import
+import os
+import pprint
+import requests
+import json
+
+# 3rd party imports
+import meetup.api
+
+
+MEETUP_API_TOKEN = os.getenv("MEETUP_API_TOKEN")
+EVENTS_ENDPOINT = os.getenv("EVENTS_ENDPOINT")
+
+client = meetup.api.Client(MEETUP_API_TOKEN)
+
+
+def get_events_by_member(member_id):
+    """ Gets events for a specific member using a given members id.
+    """
+    response = client.GetEvents({"member_id": member_id})
+    return [event for event in response.results]
+
+
+def _transform_event(event):
+    return {
+        "created": event["created"],
+        "description": event["description"],
+        "event_url": event["event_url"],
+        "photo_url": event.get("photo_url", ""),
+        "id": event["id"],
+        "name": event["name"],
+        "group_name": event["group"]["name"],
+        "member_type": event["group"]["name"],
+        "status": event["status"],
+        "time": event["time"],
+    }
+
+
+def _post_payloads(payloads):
+    responses = []
+    for payload in payloads:
+        r = requests.post(
+            EVENTS_ENDPOINT,
+            headers={"Content-type": "application/json"},
+            data=json.dumps(payload),
+        )
+
+        responses.append(r)
+
+
+if __name__ == "__main__":
+    member_id = "135086862"
+
+    events = get_events_by_member(member_id)
+    transformed_events = [_transform_event(e) for e in events]
+
+    _post_payloads(transformed_events)
