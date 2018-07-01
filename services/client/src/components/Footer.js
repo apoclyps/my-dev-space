4e57ab7e2aecd584260c7606872b8d0fc03b9162
Alistair Brown <github@alistairjcbrown.com>
2018-07-01 13:17:58 +0100

    Set up JS linting

--- services/client/src/components/Footer.js
+++ services/client/src/components/Footer.js
@@ -1,21 +1,17 @@
 import React from "react";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div className="bg-blue lg:mb-4 p-2 rounded-b text-xs text-white">
-        <div>
-          &copy; 2018 | Site by
-          <a
-            className="bg-blue text-white rounded no-underline p-1"
-            href="https://twitter.com/apoclyps"
-          >
-            <i className="fa fa-twitter" /> Kyle Harrison
-          </a>
-        </div>
-      </div>
-    );
-  }
-}
+const Footer = () => (
+  <div className="bg-blue lg:mb-4 p-2 rounded-b text-xs text-white">
+    <div>
+      &copy; 2018 | Site by
+      <a
+        className="bg-blue text-white rounded no-underline p-1"
+        href="https://twitter.com/apoclyps"
+      >
+        <i className="fa fa-twitter" /> Kyle Harrison
+      </a>
+    </div>
+  </div>
+);
 
 export default Footer;
